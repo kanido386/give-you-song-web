bdb073ed3cc889829b452c7d3af6807fd9aef715
kanido386 <kanido386@gmail.com>
2022-04-11 12:24:55 +0800

    Can get tracks from chart, but there are a lot of bugs...

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+// for Spotify
+// require 'vendor/autoload.php';
+
+// FIXME:
+// https://stackoverflow.com/questions/9650090/undefined-variable-session
+session_start();
+
 use Illuminate\Support\Facades\Route;
 
 use GuzzleHttp\Client;
@@ -42,6 +49,50 @@ class AccessToken {
     }
 }
 
+class SpotifyService {
+
+    protected $SpotifyClientID = null;
+    protected $SpotifyClientSecret = null;
+    protected $RedirectURI = "http://localhost/";
+    
+    public $api = null;
+    protected $accessToken = null;
+
+    public function __construct()
+    {
+        $this->SpotifyClientID = env('SPOTIPY_CLIENT_ID');
+        $this->SpotifyClientSecret = env('SPOTIPY_CLIENT_SECRET');
+
+        $session = new SpotifyWebAPI\Session(
+            $this->SpotifyClientID,
+            $this->SpotifyClientSecret,
+            $this->RedirectURI
+        );
+
+        $api = new SpotifyWebAPI\SpotifyWebAPI();
+
+        if (isset($_GET['code'])) {
+            $session->requestAccessToken($_GET['code']);
+            $this->accessToken = $session->getAccessToken();
+            $api->setAccessToken($this->accessToken);
+        
+            // var_dump($api->me());
+        } else {
+            $options = [
+                'scope' => [
+                    'user-read-email',
+                ],
+            ];
+        
+            header('Location: ' . $session->getAuthorizeUrl($options));
+            die();
+        }
+    }
+}
+
+// TODO:
+$api = null;
+// $GLOBALS['api'] = null;
 
 class OpenAPI {
     protected $clientID = null;
@@ -101,6 +152,31 @@ class OpenAPI {
         $url = $this->API_END_POINT . "/charts?territory=$territory&offset=$offset&limit=$limit";
         return $this->fetch($url);
     }
+
+    public function fetchTracksInChart($playlistID, $territory = Territory::Taiwan, $offset = 0, $limit = 30)
+    {
+        $url = $this->API_END_POINT . "/charts/$playlistID/tracks?territory=$territory&offset=$offset&limit=$limit";
+        return $this->fetch($url);
+    }
+
+    public function getCleanerName($name)
+    {
+        $cleanerName = $name;
+
+        // turn
+        // "想見你想見你想見你 (Miss You 3000) - 電視劇<想見你>片尾曲"
+        // to
+        // "想見你想見你想見你 (Miss You 3000)"
+        $cleanerName = explode(" -", $cleanerName)[0];
+
+        // turn
+        // "想見你想見你想見你 (Miss You 3000)"
+        // to
+        // "想見你想見你想見你"
+        $cleanerName = explode(" (", $cleanerName)[0];
+
+        return $cleanerName;
+    }
 }
 
 Route::get('/', function () {
@@ -132,10 +208,67 @@ Route::get('/', function () {
     return view('welcome', ['charts' => $randomCharts]);
 });
 
-Route::get('/test', function () {
-
+Route::get('/test/{id}', function ($id) {
     
+    $clientID = env('KKBOX_CLIENT_ID');
+    $clientSecret = env('KKBOX_CLIENT_SECRET');
+    $openAPI = new OpenAPI($clientID, $clientSecret);
+    $openAPI->fetchAndUpdateAccessToken();
 
-    return view('test');
+    $response = $openAPI->fetchTracksInChart($id);
+    $results = json_decode($response->getBody(), true);
+    // $out = new \Symfony\Component\Console\Output\ConsoleOutput();
+    // $out->writeln(serialize($results));
+
+    $tracks = array();
+    $index = 0;
+    foreach ($results["data"] as $item) {
+        $artistName = $openAPI->getCleanerName($item["album"]["artist"]["name"]);
+        $songName = $openAPI->getCleanerName($item["name"]);
+        $tracks[$index] = array($artistName, $songName);
+        $index++;
+    }
+
+    // FIXME: bad way
+    $num = 5;
+    $randomIndex = array_rand($tracks, $num);
+    $randomTracks = array();
+    foreach ($randomIndex as $i => $index) {
+        $randomTracks[$index] = $tracks[$index];
+    }
+    var_dump($randomTracks);
+
+
+    // FIXME:
+    // global $api;
+    $query = "盧廣仲";
+    $type = "track";
+    // $api = Session::get('api');
+    // https://stackoverflow.com/questions/1442177/storing-objects-in-php-session
+    $api = unserialize($_SESSION['api']);
+    var_dump($api->search($query, $type));
+    // var_dump($GLOBALS['api']->search($query, $type));
+
+    return view('test', ['tracks' => $randomTracks]);
 })->name('test');
 // https://laravel.com/docs/master/routing#named-routes
+
+
+Route::get('/login', function () {
+
+    // FIXME:
+    // global $api;
+    $api = new SpotifyService();
+    // $GLOBALS['api'] = new SpotifyService();
+    // Session::put('api', $api);
+
+    // https://stackoverflow.com/questions/1442177/storing-objects-in-php-session
+    $_SESSION['api'] = serialize($api);
+
+    return redirect('/');
+});
+
+// Route::get('/wow/{id}', function ($id) {
+
+//     return view('test', ['tracks' => $randomTracks]);
+// })->name('test');
